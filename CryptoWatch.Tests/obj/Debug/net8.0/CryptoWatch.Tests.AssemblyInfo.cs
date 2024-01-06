4397e005e6043ecf2b4756f63d032618cb8ac36b
ABrandaoL <alexandre.brandao.lustosa@gmail.com>
2024-01-06 12:04:12 -0300

    Redis Ok, Migration 80% Error on table

--- CryptoWatch.Tests/obj/Debug/net8.0/CryptoWatch.Tests.AssemblyInfo.cs
+++ CryptoWatch.Tests/obj/Debug/net8.0/CryptoWatch.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoWatch.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ad554ccd55485e571621e4d575408a5cca0663")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoWatch.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoWatch.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CryptoWatch/Repository/Contexts/CryptoWatchSpotContext.cs
+++ CryptoWatch/Repository/Contexts/CryptoWatchSpotContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace CryptoWatch.Repository.Domain;
 
@@ -8,6 +9,7 @@ public class CryptoWatchSpotContext : DbContext
     
     public CryptoWatchSpotContext(DbContextOptions<CryptoWatchSpotContext> options) : base(options)
     {
+        //Database.EnsureCreated(); 
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -15,5 +17,25 @@ public class CryptoWatchSpotContext : DbContext
         modelBuilder.Entity<SymbolWatch>().ToTable("SymbolWatch");
 
         base.OnModelCreating(modelBuilder);
+    }
+    
+    public class CryptoWatchSpotContextFactory : IDesignTimeDbContextFactory<CryptoWatchSpotContext>
+    {
+        public CryptoWatchSpotContext CreateDbContext(string[] args)
+        {
+            //var connectionString = Environment.GetEnvironmentVariable("CRYPTOWATCHSPOTCONNECTION");
+            var connectionString =  "Server=localhost,1433;Initial Catalog=CryptoWatchSpot;Persist Security Info=False;User ID=sa;Password=CryptoW@tch2024;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;";
+            
+            var optionsBuilder = new DbContextOptionsBuilder<CryptoWatchSpotContext>();
+            
+            Console.WriteLine("1---------");
+            Console.WriteLine(connectionString);
+            Console.Write("2---------");
+    
+           
+            optionsBuilder.UseSqlServer(connectionString);
+    
+            return new CryptoWatchSpotContext(optionsBuilder.Options);
+        }
     }
 }
